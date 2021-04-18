188b9d6e21f1face27c22229eee8df01d8dcf682
Victor Nițu <victor@thesponge.eu>
2021-04-18 16:06:51 +0300

    Fix JS srolling

--- js/front.js
+++ js/front.js
@@ -32,25 +32,27 @@ function utils() {
         window.location = $(this).find('a').attr('href');
     });
     /* external links in new window*/
-
     $('.external').on('click', function (e) {
-
         e.preventDefault();
         window.open($(this).attr("href"));
     });
+
     /* animated scrolling */
     $('.scroll-to, #navigation a').click(function (event) {
       //if ($(this).attr('href').search('/#') >= 0) return;
-
         var full_url = this.href;
+        console.log('full url ', full_url)
         var parts = full_url.split("#");
+        console.log('parts', parts)
         if (parts.length > 1) {
           event.preventDefault();
           var trgt = parts[1];
-          if (document.getElementById(trgt))
-            $('body').scrollTo($('#' + trgt), 800, {offset: -50});
-          else
-            window.location = full_url;
+          if (document.getElementById(trgt)) {
+              console.log('scrolling to ', trgt)
+              $('body').scrollTo($('#' + trgt), 800, {offset: -50});
+          } else {
+              window.location = full_url;
+          }
 
         } else {
           console.log(2);
